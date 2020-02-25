45a9a8eafdfc4cf536b3f1d94e26c11048cc9fdc
Gianfranco <crisgianfrancojuarez306@gmail.com>
2020-02-24 20:10:24 -0600

    Dotnet course

---
+++ dotnet/myApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace classes
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var account = new BankAccount("Cristian", 1000);
+      account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+      // Console.WriteLine("Balance " + account.Balance);
+      account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+      // Console.WriteLine("Balance " + account.Balance);
+
+      Console.WriteLine(account.GetAccountHistory());
+    }
+  }
+}
